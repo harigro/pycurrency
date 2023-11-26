6f9760b635fa0aec31e01d7a59b0f4462fe71be6
harigro <trierbank@gmail.com>
2023-11-26 20:30:01 +0700

    Memperbaiki kesalahan penulisan

--- __init__.py
+++ __init__.py
@@ -1,12 +1,1 @@
 from pycurrency.mata_uang import mataUang
-import pprint
-
-
-data = mataUang()
-
-if __name__=="__main__":    
-    pprint.pprint(data.semua, sort_dicts=False)
-    print()
-    pprint.pprint(data.cari("idr"), sort_dicts=False)
-    print()
-    print(len(data.semua))
